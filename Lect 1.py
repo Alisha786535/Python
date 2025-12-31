440824f9813653971c2eff10d375ae79ee01870a
Alisha786535 <alisha786535@gmail.com>
2025-12-31 16:38:46 +0500

    Create a basic Python program for learning
    This program demonstrates basic Python concepts including data types, arithmetic operations, relational operators, assignment operators, logical operators, type casting, and input methods.

---
+++ Lect 1.py
@@ -0,0 +1,68 @@
+# SMIT-DS&AI-BATCH 10
+"""MY FIRST PROGRAM O PYTHON"""
+print("Hello World! ALISHA")
+A=2
+B=4
+print(A+B)
+"""CHECK DATA TYPES BY PRINT TYPE"""
+maths=97
+english=78.75
+print(type(maths))
+print(type(english))
+criteria=True
+disability=None
+print(type(criteria))
+print(type(disability))
+"""ARITHEMATIC OPERATORS"""
+num1=5
+num2=15
+print("Add :", num1+ num2)
+print("Sub :", num2- num1)
+print("Multiplication :", num1* num2)
+print("Division :", num2/ num2)
+print("Integer division :", num2 //num1)
+print("Exponent :", num2 **num1)
+"""RELATIONAL OPERATORS"""
+num3=3
+num4=5
+print("Equality/comparision opertor :", num3 ==num4)
+print("Not Equal to operator :", num3 !=num4)
+print("greater than :", num3>num4)
+print("less than :", num3<num4)
+"""ASSINGMENT OPERATOR"""
+a=7
+a+=3
+print("Answer :", a)
+a-=3
+print("Answer :", a)
+a*=3
+print("Answer :", a)
+a/=3
+print("Answer :", a)
+a//=3
+print("Answer :", a)
+a%=3
+print("Answer :", a)
+"""LOGICAL OPERATOR"""
+num5=5
+num6=5
+print(num5!=num6 and num5==num6)
+print(num5<num6 or num5>num6)
+print(not(num5==num6))
+# type conversion
+a=2
+b=4.5
+print(a+b)
+"""TYPE CASTING"""
+c=2
+d=4.5
+result=c+int(d)
+print(result)
+"""Input methods"""
+name =input("Enter your name: ")
+age=input("Enter your age: ")
+print("Thank you ",name)
+print("your age is ",age)
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+print("Add : ",num1+num2)
